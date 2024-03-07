347ad0e6f12d158423e710fef60c6f874712ea9d
doondisunkara <sunkaranag2004@gmail.com>
2024-03-07 15:40:40 +0000

    App completed

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,20 +1,24 @@
+import {Link} from 'react-router-dom'
+
 import './index.css'
 
 const BlogItem = props => {
   const {blogDetails} = props
-  const {author, topic, title, avatarUrl, imageUrl} = blogDetails
+  const {id, author, topic, title, avatarUrl, imageUrl} = blogDetails
   return (
-    <div className="blog-item">
-      <img src={imageUrl} alt="i" className="book-img" />
-      <div className="book-details">
-        <p className="book-topic">{topic}</p>
-        <h1 className="book-title">{title}</h1>
-        <div className="author-container">
-          <img src={avatarUrl} alt="avatar" className="author-avatar" />
-          <p className="author-name">{author}</p>
+    <Link to={`/blogs/${id}`} className="blog-item-link">
+      <div className="blog-item">
+        <img src={imageUrl} alt={title} className="book-img" />
+        <div className="book-details">
+          <p className="book-topic">{topic}</p>
+          <h1 className="book-title">{title}</h1>
+          <div className="author-container">
+            <img src={avatarUrl} alt="avatar" className="author-avatar" />
+            <p className="author-name">{author}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
--- src/components/BlogItemDetails/index.js
+++ src/components/BlogItemDetails/index.js
@@ -1,1 +1,50 @@
-// Write your JS code here
+import {Component} from 'react'
+
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}}
+
+  componentDidMount() {
+    this.getBlogItemDetails()
+  }
+
+  getBlogItemDetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    const updatedData = {
+      author: data.author,
+      id: data.id,
+      imageUrl: data.image_url,
+      avatarUrl: data.avatar_url,
+      title: data.title,
+      content: data.content,
+      topic: data.topic,
+    }
+    console.log(updatedData)
+    this.setState({blogData: updatedData})
+  }
+
+  render() {
+    const {blogData} = this.state
+    const {id, author, content, topic, title, avatarUrl, imageUrl} = blogData
+    return (
+      <div className="blog">
+        <h1 className="blog-title">{title}</h1>
+        <div className="blog-book-container">
+          <div className="blog-author-container">
+            <img src={avatarUrl} alt={id} className="blog-author-avatar" />
+            <h1>{author}</h1>
+          </div>
+          <img src={imageUrl} alt={title} className="blog-image" />
+          <p className="blog-content">{content}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BlogItemDetails
